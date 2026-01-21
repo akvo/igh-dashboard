eed7201d7ac05917a973dad534ee9702ac2d68af
zuhdil <zuhdil@gmail.com>
2026-01-21 16:48:51 +0700

    Add VM deployment setup

---
+++ self-hosted/generate_dynamic_config.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Remove 'https://' from WEBDOMAIN if present
+WEBDOMAIN=${WEBDOMAIN#https://}
+
+cat << EOF > /traefik-config/dynamic.yml
+http:
+  routers:
+    frontend-service-router-80:
+      rule: "Host(\`${WEBDOMAIN}\`)"
+      service: frontend-service
+      entrypoints: web
+      middlewares:
+        - redirect-to-https
+
+    frontend-service-router-443:
+      entrypoints:
+        - websecure
+      rule: "Host(\`${WEBDOMAIN}\`)"
+      service: frontend-service
+      tls:
+        certResolver: myresolver
+
+  middlewares:
+    redirect-to-https:
+      redirectScheme:
+        scheme: "https"
+        permanent: true
+
+
+  services:
+    frontend-service:
+      loadBalancer:
+        servers:
+          - url: "http://frontend:3000"
+
+EOF
